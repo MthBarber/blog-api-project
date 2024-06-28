a8452d387a94c1c8d6d65487f41d4bdadfbe522f
mthbarber <mthbarber@gmail.com>
2024-06-28 15:46:47 +0100

    Create project with node and express and add home root

---
+++ backend-api/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send("Hello world")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`)
+})

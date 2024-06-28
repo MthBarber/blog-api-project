f0f9745a4685b1f752f3f3c61aab3bc1a6b50f51
mthbarber <mthbarber@gmail.com>
2024-06-28 16:55:27 +0100

    Begin adding routing for blogposts to get all posts and individual posts

--- backend-api/index.js
+++ backend-api/index.js
@@ -1,13 +1,36 @@
 require('dotenv').config()
 
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
     res.send("Hello world")
+});
+
+app.get('/blogposts',(req,res) => {
+    res.send(blogposts)
+})
+
+app.get('/blogposts/:postId',(req,res) => {    
+    return res.send(blogposts[req.params.postId])
 })
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
-})
+});
+
+
+const blogposts = [
+    
+   {
+      "postId": 0, 
+      "postBody": "This is a blog post by me",
+      "author": "Random person",
+    },
+    {
+      "postId": 1, 
+      "postBody": "This is a another blog post by me",
+      "author": "Random person",
+    }
+
+]
